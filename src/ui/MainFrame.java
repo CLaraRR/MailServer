f2eafc8dd713f42a174b3863d83af3fe5fc25c85
CLaraRR <176367137@qq.com>
2017-05-19 18:56:13 +0800

    2017/5/19

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -6,13 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+
 import java.awt.GridLayout;
+
 import javax.swing.JTabbedPane;
+
 import java.awt.Color;
+
 import javax.swing.JSeparator;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -20,7 +27,33 @@ import javax.swing.JTextField;
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField IPtextField;
+	private JTextField PorttextField;
+	private JMenuBar menuBar;
+	private JMenu menuFile;
+	private JMenu menuHelp;
+	private JTabbedPane tabbedPane;
+	private JPanel panelControl;
+	private JPanel panelUser;
+	private JPanel panelLog;
+	private JPanel panelSystem;
+	private JLabel lblIp;
+	private JLabel lblPort;
+	private JLabel lblmailserver;
+	private JLabel lblip;
+	private JButton refreshButton;
+	private JButton startButton;
+	private JLabel lblsmtp;
+	private JLabel lblsmtpstate;
+	private JTextField smtpstateTextField;
+	private JButton startSMTPButton;
+	private JLabel lblpop;
+	private JLabel lblpopstate;
+	private JTextField popstateTextField;
+	private JButton startPOPButton;
+	
+	
+	
 
 	/**
 	 * Launch the application.
@@ -38,61 +71,133 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 660);
 		
-		JMenuBar menuBar = new JMenuBar();
+		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu menu = new JMenu("文件");
-		menuBar.add(menu);
+		menuFile = new JMenu("文件");
+		menuBar.add(menuFile);
 		
 		JMenuItem menuItem = new JMenuItem("退出");
-		menu.add(menuItem);
+		menuFile.add(menuItem);
 		
-		JMenu menu_1 = new JMenu("帮助");
-		menuBar.add(menu_1);
+		menuHelp = new JMenu("帮助");
+		menuBar.add(menuHelp);
 		
 		JMenuItem menuItem_1 = new JMenuItem("关于邮件服务器");
-		menu_1.add(menuItem_1);
+		menuHelp.add(menuItem_1);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(0, 0, 874, 29);
 		contentPane.add(tabbedPane);
 		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("控制面板", null, panel, null);
+		panelControl = new JPanel();
+		tabbedPane.addTab("控制面板", null, panelControl, null);
 		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("用户管理", null, panel_1, null);
+		panelUser = new JPanel();
+		tabbedPane.addTab("用户管理", null, panelUser, null);
 		
-		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab("日志管理", null, panel_2, null);
+		panelLog = new JPanel();
+		tabbedPane.addTab("日志管理", null, panelLog, null);
 		
-		JPanel panel_3 = new JPanel();
-		tabbedPane.addTab("系统管理", null, panel_3, null);
+		panelSystem = new JPanel();
+		tabbedPane.addTab("系统管理", null, panelSystem, null);
+		
+		/**
+		 * IP地址部分
+		 */
+		lblIp = new JLabel("IP地址");
+		lblIp.setBounds(24, 39, 54, 15);
+		contentPane.add(lblIp);
+		
+		lblmailserver = new JLabel("欢迎使用MailServer，版本号1.0");
+		lblmailserver.setBounds(64, 64, 277, 15);
+		contentPane.add(lblmailserver);
+		
+		lblip = new JLabel("服务器当前IP地址");
+		lblip.setBounds(64, 109, 114, 15);
+		contentPane.add(lblip);
+		
+		IPtextField = new JTextField();
+		IPtextField.setBounds(176, 106, 93, 21);
+		contentPane.add(IPtextField);
+		IPtextField.setColumns(10);
+		
+		lblPort = new JLabel("服务器当前端口");
+		lblPort.setBounds(300, 109, 114, 15);
+		contentPane.add(lblPort);
+		
+		PorttextField = new JTextField();
+		PorttextField.setBounds(420, 106, 93, 21);
+		contentPane.add(PorttextField);
+		PorttextField.setColumns(10);
+		
+		refreshButton=new JButton();
+		refreshButton.setBounds(600, 106, 93, 21);
+		refreshButton.setText("刷新IP");
+		contentPane.add(refreshButton);
+		
+		startButton=new JButton();
+		startButton.setBounds(700, 106, 100, 21);
+		startButton.setText("启动服务器");
+		contentPane.add(startButton);
+		
 		
 		JSeparator separator = new JSeparator();
 		separator.setBounds(10, 155, 864, 2);
 		contentPane.add(separator);
 		
-		JLabel lblIp = new JLabel("IP\u5730\u5740");
-		lblIp.setBounds(24, 39, 54, 15);
-		contentPane.add(lblIp);
+		/**
+		 * SMTP协议部分
+		 */
+		lblsmtp=new JLabel("SMTP协议");
+		lblsmtp.setBounds(24, 160, 60, 15);
+		contentPane.add(lblsmtp);
 		
-		JLabel lblmailserver = new JLabel("\u6B22\u8FCE\u4F7F\u7528MailServer,\u5F53\u524D\u7248\u672C1.0");
-		lblmailserver.setBounds(64, 64, 277, 15);
-		contentPane.add(lblmailserver);
+		lblsmtpstate=new JLabel("SMTP协议状态");
+		lblsmtpstate.setBounds(64, 184, 114, 15);
+		contentPane.add(lblsmtpstate);
 		
-		JLabel lblip = new JLabel("\u670D\u52A1\u5668\u5F53\u524DIP\u5730\u5740\uFF1A");
-		lblip.setBounds(64, 109, 114, 15);
-		contentPane.add(lblip);
+		smtpstateTextField=new JTextField();
+		smtpstateTextField.setBounds(176, 184, 93, 21);
+		//smtpstateTextField.setBackground();
+		contentPane.add(smtpstateTextField);
+		smtpstateTextField.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setBounds(176, 106, 93, 21);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		startSMTPButton=new JButton();
+		startSMTPButton.setBounds(300, 184, 120, 21);
+		startSMTPButton.setText("启动SMTP协议");
+		contentPane.add(startSMTPButton);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(10, 250, 864, 2);
+		contentPane.add(separator_1);
+		
+		/**
+		 * POP3协议
+		 */
+		lblpop=new JLabel("POP3协议");
+		lblpop.setBounds(24, 300, 60, 15);
+		contentPane.add(lblpop);
+		
+		lblpopstate=new JLabel("POP3协议状态");
+		lblpopstate.setBounds(64, 324, 114, 15);
+		contentPane.add(lblpopstate);
+		
+		popstateTextField=new JTextField();
+		popstateTextField.setBounds(176, 324, 93, 21);
+		//smtpstateTextField.setBackground();
+		contentPane.add(popstateTextField);
+		popstateTextField.setColumns(10);
+		
+		startPOPButton=new JButton();
+		startPOPButton.setBounds(300, 324, 120, 21);
+		startPOPButton.setText("启动POP3协议");
+		contentPane.add(startPOPButton);
+
 		
 		
 	}
